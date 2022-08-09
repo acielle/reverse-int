cee7faa64de577529a1f6dba10e09f299555e4d0
acielle <sg23mj@gmail.com>
2022-08-09 22:50:22 +0300

    init: start reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0 ) n = Math.abs(n);
+    const nStr = n.toString();
+    const result = nStr.split('').reverse().join('');  
+    return result;
 }
